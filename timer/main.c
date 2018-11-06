4dd78edc559a033165259236239f0aad0a1c234f
Stefan Schokker <stefan.schokker@hva.nl>
2018-11-06 18:12:04 +0100

    final version of counter and timer

--- timer/main.c
+++ timer/main.c
@@ -12,13 +12,15 @@
 #define RES 16  // Timer reset signal       // Red
 
 int save(int iteration, struct timespec act_high, struct timespec act_low, struct timespec ver_high,
-         struct timespec ver_low);
-char* toHuman(struct timespec machine);
+		 struct timespec ver_low);
+char * toHuman(struct timespec machine);
 
 int main()
 {
 	// Setup without remapping
 	wiringPiSetupGpio();
+
+	struct timespec act_high, act_low, ver_high, ver_low;
 
 	pinMode(VER, INPUT);
 	pinMode(ACT, INPUT);
@@ -30,11 +32,18 @@ int main()
 	printf("RES: HIGH\n");
 	digitalWrite(RES, HIGH);
 
-	for (int i = 0; i < 100; i ++)
+	for (int i = 39; i < 100; i ++)
 	{
 		printf("Waiting for arduino and counter, iteration: %d\n", i);
-		while (digitalRead(VER) == LOW || digitalRead(ACT) == LOW)
+		while (digitalRead(ACT) == LOW)
 		{ }
+
+		clock_gettime(CLOCK_MONOTONIC_RAW, &act_high);
+
+		while (digitalRead(VER) == LOW)
+		{ }
+
+		clock_gettime(CLOCK_MONOTONIC_RAW, &ver_high);
 
 		printf("Arduino and counter active, releasing RES\n");
 		digitalWrite(RES, LOW);
@@ -44,12 +53,20 @@ int main()
 		while (digitalRead(ACT) == HIGH)
 		{ }
 
+		clock_gettime(CLOCK_MONOTONIC_RAW, &act_low);
+
+		while (digitalRead(VER) == HIGH)
+		{ }
+
+		clock_gettime(CLOCK_MONOTONIC_RAW, &ver_low);
+
 		printf("Counter done, setting RES\n");
 		digitalWrite(RES, HIGH);
 
-		delay(100);
+		if (save(i, act_high, act_low, ver_high, ver_low) == 1)
+		{ printf("Error while saving\n"); }
 
-		// TODO :save timestamps
+		delay(100);
 	}
 
 
@@ -57,16 +74,14 @@ int main()
 }
 
 int save(int iteration, struct timespec act_high, struct timespec act_low, struct timespec ver_high,
-         struct timespec ver_low)
+		 struct timespec ver_low)
 {
 	FILE *fp;
-	int ret;
 
 	if (access(FPATH, F_OK) == 0)
 	{
 		// Open file in append mode if it exists
 		fp = fopen(FPATH, "a");
-		ret = 0;
 	}
 	else
 	{
@@ -76,38 +91,54 @@ int save(int iteration, struct timespec act_high, struct timespec act_low, struc
 		if (fp == NULL)
 		{
 			fprintf(stderr, "Error opening file");
-			ret = 1;
+			return 1;
 		}
 
 		// Print colons into file
-		fprintf(fp, "ACT_HIGH, ACT_LOW, VER_HIGH, VER_LOW, PLS_SND, PLS_REC\n");
+		fprintf(fp, "%s\n", "ITER_CNT, ACT_HIGH, ACT_LOW, VER_HIGH, VER_LOW, PLS_SND, PLS_REC");
 	}
 
-	if (ret == 0)
-	{
-		char *ah = toHuman(act_high),
-			 *al = toHuman(act_low),
-			 *vh = toHuman(ver_high),
-			 *vl = toHuman(ver_low),
-			 *ps, *pr = "10000000"; // Target from "../counter/src/main.c:6"
+	char *ah = toHuman(act_high),
+		 *al = toHuman(act_low),
+		 *vh = toHuman(ver_high),
+		 *vl = toHuman(ver_low),
+		 ct[3], ps[24], *pr = "10000000"; // Target from "../counter/src/main.c:6"
 
-		// According to "../docs/length_commit_3.bmp", the frequency of the arduino's signal is about 177kHz
-		int freq = 177000;
-		// Duration in seconds
-		double duration = ver_high.tv_nsec - ver_low.tv_nsec / 1000000000.0;
+	sprintf(ct, "%d", iteration);
 
-		sprintf(ps, "%lf", freq * duration);
+	// According to "../docs/length_commit_3.bmp", the frequency of the arduino's signal is about 177kHz
+	int freq = 177000;
+	// Duration in seconds
+	double duration = (ver_low.tv_sec - ver_high.tv_sec) * 1000000 + (ver_low.tv_nsec - ver_high.tv_nsec) / 1000;
+	// Convert to string
+	sprintf(ps, "%lf", freq * duration);
 
-	}
+	// Write data to file
+	fprintf(fp, "%s, ", ct); // Count
+	fprintf(fp, "%s, ", ah); // ACT_HIGH
+	fprintf(fp, "%s, ", al); // ACT_LOW
+	fprintf(fp, "%s, ", vh); // VER_HIGH
+	fprintf(fp, "%s, ", vl); // VER_LOW
+	fprintf(fp, "%s, ", ps); // Pulses send
+	fprintf(fp, "%s\n", pr); // Pulses counted
 
-	return ret;
+	fclose(fp);
+
+	// Free memory that we allocated in toHuman();
+	free(ah);
+	free(al);
+	free(vh);
+	free(vl);
+
+	return 0;
 }
 
-char* toHuman(struct timespec machine)
+char * toHuman(struct timespec machine)
 {
-	struct tm *ptm = gmtime(&machine);
-	char buf[8];
-	strftime(buf, sizeof buf, "%T", ptm);
-	return &buf;
+//	struct tm *ptm = gmtime(&machine.tv_nsec);
+	char *buf = malloc(64);
+	sprintf(buf, "%lld.%.9ld", (long long)machine.tv_sec, machine.tv_nsec);
+//	strftime(buf, sizeof buf, "%T", ptm);
+	return buf;
 }
 
